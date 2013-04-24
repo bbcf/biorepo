3b87656cdd2e64b6954fa4181d2bc164e925f292
Yoann Mouscaz <ymouscaz@hotmail.fr>
2013-04-24 17:10:16 +0200

    add searchbar in Projects/Samples/Measurements grids

---
+++ biorepo/public/javascript/search_in_grid.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+
+    /* Init the table */
+    var oTable = $('.grid').dataTable( {
+    });
+
+
+
+    // /* $('.row_selected') */
+    // /* customize interface searchpage*/
+    // $('.dataTables_info').first().remove();
+    // /*$('.dataTables_paginate').first().remove();*/
+    // $('.paging_two_button').first().remove();
+
+} );
+
+
