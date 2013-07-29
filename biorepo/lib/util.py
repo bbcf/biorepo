11b8d4f1ca2253c337cc5e264b02cb636a9a324e
Yoann Mouscaz <ymouscaz@hotmail.fr>
2013-07-29 16:53:20 +0200

    bug fixed with filename in content-disposition 2

--- biorepo/lib/util.py
+++ biorepo/lib/util.py
@@ -99,11 +99,11 @@ def sha1_generation_controller(local_path, url_path, url_bool, tmp_dirname):
             try:
                 infos = u.info().get('Content-Disposition')
                 #filename = re.search('filename=(?P<name>\S+);', infos).group('name')
-                filename = re.search(('filename="(?P<name>?.+?(;|$))'), infos).group('name')
+                filename = re.search(('filename=(?P<name>"?.+?(;|$))'), infos).group('name')
                 if not filename:
                     filename = url_path.split('/')[-1]
                 if filename[-1] == '"':
-                    filename = filename[:-1] 
+                    filename = filename[1:-1]
             except:
                 filename = url_path.split('/')[-1]
 
