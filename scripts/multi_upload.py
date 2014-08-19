4a790a41fe96f9a41536a1df6fb4ed8b26440126
Yoann Mouscaz <ymouscaz@hotmail.fr>
2014-08-19 10:32:29 +0200

    fix str/int pb with spreadsheet for project_id

--- scripts/multi_upload.py
+++ scripts/multi_upload.py
@@ -177,9 +177,9 @@ def run_script(root, path_tgz):
 
         if dico_project["project_id"]:
             #if Excel transform string in float...
-            p_id_tmp = (dico_project["project_id"]).split('.')
-            p_id = p_id_tmp[0]
+            p_id = int(float(dico_project["project_id"]))
             PROJECT['project_id'] = p_id
+            print "------------- ", p_id, " GIVEN PROJECT ID -------------"
         else:
             PROJECT['project_id'] = ""
 
