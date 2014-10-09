59b0d2f81d2504489bdc18071bcf1ea4d368b836
Yoann Mouscaz <ymouscaz@hotmail.fr>
2014-10-09 15:18:21 +0200

    edit trackhub colors : debug

--- biorepo/controllers/trackhub.py
+++ biorepo/controllers/trackhub.py
@@ -103,9 +103,12 @@ class TrackhubController(BaseController):
 
             t_length = len(dic_colors.keys())
             edit_form = build_form_edit_th(t_length)(action=url('/trackhubs/post_edit')).req()
+            print edit_form, "----edit_form"
+            print edit_form.child, "---child"
+            print edit_form.child.children, "----children"
             for c in edit_form.child.children:
-                print c
-                print edit_form.child.children[0]
+                print c, "---c"
+                print edit_form.child.children[0], "---normal"
                 print c[0]
                 # for k, v in dic_colors.items():
                 #     #even --> track
